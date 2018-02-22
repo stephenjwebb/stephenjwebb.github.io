4d627c240a1abf5bb7192900639dcd9c36b2b152
Stephen <stephenwebb1994@gmail.com>
2018-02-22 02:09:04 -0800

    added if statements

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,7 +8,24 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
-         text_area.textContent = compassHeading;
+
+  if(compassHeading > 315 || compassHeading < 45) {
+                text_area.textContent = compassHeading;
+        }
+
+        if(compassHeading >= 45 && compassHeading < 135) {
+                text_area.textContent = "To the echelon, the workstation.";
+        }
+
+        if(compassHeading >= 135 && compassHeading < 225) {
+                text_area.textContent = "The workhouses";
+        }
+
+        if(compassHeading >= 225 && compassHeading < 315) {
+                text_area.textContent = "The worked";
+        }
+
+        // text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
     }
